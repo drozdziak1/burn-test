667868906d60053575673c90e1fd135cd431d8bc
Stan Drozd <drozdziak1@gmail.com>
2025-05-23 20:51:28 +0200

    data_helpers: Add a BatchStrategy for packing fineweb texts

--- src/data_helpers.rs
+++ src/data_helpers.rs
@@ -1,10 +1,13 @@
 use anyhow::{Result, anyhow, bail};
+use burn::data::dataloader::BatchStrategy;
 use hf_hub::api::sync::ApiRepo;
 use log::{debug, warn};
 use polars::prelude::*;
 use tokenizers::Tokenizer;
 
-use std::path::PathBuf;
+use std::{num::NonZeroUsize, path::PathBuf};
+
+use burn::data::dataset::HuggingfaceDatasetLoader;
 
 /// Visits subsequent parquet files of the fineweb dataset; tracks state for
 /// returning subsequent records
@@ -142,7 +145,8 @@ impl<'a> Iterator for BatchIter<'a> {
     // Tensor::from_vec() expects flat vecs
     type Item = Vec<Vec<u32>>;
     fn next(&mut self) -> Option<Self::Item> {
-        let mut flat_mbatches = vec![Vec::with_capacity(self.mbatch_size * self.sample_size); self.batch_size];
+        let mut flat_mbatches =
+            vec![Vec::with_capacity(self.mbatch_size * self.sample_size); self.batch_size];
 
         for (mbatch_bufs, flat_mbatch) in self.buffers.iter_mut().zip(flat_mbatches.iter_mut()) {
             for buf in mbatch_bufs.iter_mut() {
@@ -171,5 +175,77 @@ impl<'a> Iterator for BatchIter<'a> {
         }
 
         Some(flat_mbatches)
+    }
+}
+
+pub struct PackedBatchStrategy<T> {
+    batch_size: NonZeroUsize,
+    batch_item_size: usize,
+    delim: Option<T>,
+    buffers: Vec<Vec<T>>,
+}
+
+impl<T: Clone> PackedBatchStrategy<T> {
+    pub fn new(batch_size: NonZeroUsize, batch_item_size: usize, delim: Option<T>) -> Self {
+        Self {
+            batch_size,
+            batch_item_size,
+            delim,
+            buffers: vec![Vec::new(); batch_size.into()],
+        }
+    }
+}
+
+impl<T: Send + Clone + 'static> BatchStrategy<Vec<T>> for PackedBatchStrategy<T> {
+    fn add(&mut self, mut item: Vec<T>) {
+        let shortest_buf = self
+            .buffers
+            .iter_mut()
+            .min_by(|x, y| x.len().cmp(&y.len()))
+            .expect(
+                "INTERNAL: PackedBatchStrategy: a shortest vector in self.buffers does not exist, despite batch_size: NonZeroUsize",
+            );
+
+        if !shortest_buf.is_empty() {
+            if let Some(delim) = self.delim.as_ref() {
+                shortest_buf.push(delim.clone());
+            }
+        }
+
+        shortest_buf.append(&mut item);
+    }
+
+    fn batch(&mut self, force: bool) -> Option<Vec<Vec<T>>> {
+        // Cheap: move on if force
+        if !force {
+            // Less cheap: check if we have enough data for a batch
+            let any_buf_too_short = self
+                .buffers
+                .iter()
+                .any(|buf| buf.len() < self.batch_item_size);
+
+            if any_buf_too_short {
+                return None;
+            }
+        }
+
+        let mut batch: Vec<Vec<T>> = Vec::with_capacity(self.batch_size.into());
+
+        for (buf, batch_item) in self.buffers.iter_mut().zip(batch.iter_mut()) {
+            let (new_batch_item, new_buf) = buf.split_at(self.batch_item_size);
+
+            *batch_item = new_batch_item.to_vec();
+            *buf = new_buf.to_vec();
+        }
+
+        Some(batch)
+    }
+
+    fn clone_dyn(&self) -> Box<dyn BatchStrategy<Vec<T>>> {
+        Box::new(Self::new(
+            self.batch_size,
+            self.batch_item_size,
+            self.delim.clone(),
+        ))
     }
 }
